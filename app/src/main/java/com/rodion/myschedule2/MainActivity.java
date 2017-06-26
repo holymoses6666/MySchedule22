fe27e40213486dce5a0ace66c1765d36906cc0e8
Rodion Vagin <letmecut@gmail.com>
2017-06-26 10:06:04 +0300

    кнопка добавления работает

--- app/src/main/java/com/rodion/myschedule2/MainActivity.java
+++ app/src/main/java/com/rodion/myschedule2/MainActivity.java
@@ -3,12 +3,11 @@ package com.rodion.myschedule2;
 import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup.LayoutParams;
+import android.widget.Button;
 import android.widget.LinearLayout;
-import android.widget.TextView;
+
 
 public class MainActivity extends Activity {
 
@@ -19,6 +18,7 @@ public class MainActivity extends Activity {
         int salary[] = { 13000, 10000, 13000, 13000, 10000, 15000, 13000, 8000 , 1, 2, 3, 4};
 
         int[] colors = new int[2];
+        Button CreatePunktBtn;
 
         /** Called when the activity is first created. */
         public void onCreate(Bundle savedInstanceState) {
@@ -28,10 +28,22 @@ public class MainActivity extends Activity {
                 colors[0] = Color.parseColor("#559966CC");
                 colors[1] = Color.parseColor("#55336699");
 
-                LinearLayout linLayout = (LinearLayout) findViewById(R.id.linLayout);
+                final LinearLayout linLayout = (LinearLayout) findViewById(R.id.linLayout);
 
-                LayoutInflater ltInflater = getLayoutInflater();
+                final LayoutInflater ltInflater = getLayoutInflater();
 
+                CreatePunktBtn = (Button) findViewById(R.id.CreatePunktBtn);
+
+                CreatePunktBtn.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                                View item = ltInflater.inflate(R.layout.item, linLayout, false);
+                                item.getLayoutParams().width = LinearLayout.LayoutParams.MATCH_PARENT;
+                                item.setBackgroundColor(Color.parseColor("#55336699"));
+                                linLayout.addView(item);
+
+                        }
+                });/*
                 for (int i = 0; i < name.length; i++) {
                         Log.d("myLogs", "i = " + i);
                         View item = ltInflater.inflate(R.layout.item, linLayout, false);
@@ -44,6 +56,6 @@ public class MainActivity extends Activity {
                         item.getLayoutParams().width = LayoutParams.MATCH_PARENT;
                         item.setBackgroundColor(colors[i % 2]);
                         linLayout.addView(item);
-                }
+                }*/
         }
 }
